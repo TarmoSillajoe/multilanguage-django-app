6291b23de6b5a1f06f6340d04d715e419746e1fd
tarmo <tsillajoe@gmail.com>
2023-06-06 22:34:26 +0300

    In views.py, using a decorator function to allow views to activate another language

--- language_app/urls.py
+++ language_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.home_alone, name="home"),
     path("myhtmx/", views.myhtmx, name="myhtmx"),
     path("page/", views.page, name="page"),
 ]
--- language_app/views.py
+++ language_app/views.py
@@ -1,34 +1,59 @@
+from django import http
 from django.shortcuts import render
 from django.conf import settings
 
 # from django.utils import translation
-from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.translation import gettext
 from django.utils import translation
 
 
-def home(request):
+def language_switcher(func):
+    """
+    A decorator that wraps a view in a new function that checks for language-cookie presence in request.
+    If a language-change was requested i.e cookie is present, the wrapper
+    - activates the requested language,
+    - creates the response bye rendering the view, using the new language,
+    - sets the language cookie to be used in session,
+    - returns the response
+
+    Args:
+        request
+
+    Returns:
+        django.http.HttpResponse
+    """
+
+    def wrapper(request: http.HttpRequest) -> http.HttpResponse:
+        request_cookie_lang = request.COOKIES.get(
+            settings.LANGUAGE_COOKIE_NAME,
+            None,
+        )
+        if request_cookie_lang is None:
+            return func(request)
+        translation.activate(request_cookie_lang)
+        response = func(request)
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, request_cookie_lang)
+        return response
+
+    return wrapper
+
+
+@language_switcher
+def home_alone(request):
     title = gettext("Homepage")
     context = {
         "person": "John",
         "title": title,
     }
 
-    language_chosen = request.COOKIES.get(
-        settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE
-    )
-    if language_chosen:
-        translation.activate(language_chosen)
-    response = render(
+    return render(
         request,
         "home.html",
         context=context,
     )
 
-    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language_chosen)
-    return response
 
-
+@language_switcher
 def page(request):
     context = {"message": gettext("Message")}
     return render(request, "page.html", context=context)
